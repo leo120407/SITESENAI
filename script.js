940bea995d60a6d7654c468fffc8f3b4a59b80af
leo120407 <leonardoborim@gmail.com>
2023-09-21 17:46:23 -0300

    Redireciona para a pagina principal quando o nick existe e redireciona para o login quando não existe

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+function verifynickmain() {
+    if (!!localStorage.getItem("nick")) {
+        window.location.replace("principal.html")
+    }
+}
 function writetitle() {
-    let nickls = localStorage.getItem("nick")
-    let title = document.getElementsByClassName("title")[0]
-    title.innerHTML += `${nickls}!`
+    if (!!localStorage.getItem("nick")) {
+        let nickls = localStorage.getItem("nick")
+        let title = document.getElementsByClassName("title")[0]
+        title.innerHTML += `${nickls}!`
+    }
+    else {
+        window.alert("Defina seu nick")
+        window.location.replace("index.html")
+    }
+    
 }
 
 function changenick() {
